c893136dbf629f6a20e359e52be21eb194895a9d
Kvothek88 <115589647+Kvothek88@users.noreply.github.com>
2023-03-18 16:39:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,31 +4,6 @@ import numpy as np
 import random as rn
 from game_variables import *
 
-def reset_game(difficulty,game_type,mines_list): #restarts game and resets game type
-    if game_type==0:
-        dimension = 9
-    elif game_type==1:
-        dimension=16
-    elif game_type==2:
-        dimension = 30
-    game_mines_num = mines_list[game_type][difficulty]
-    mines_count = game_mines_num
-    board = np.empty((dimension,dimension),dtype='object')
-    current_board = np.empty((dimension,dimension),dtype='object')
-    for i in range(dimension):
-        board[i] = "-"
-    for i in range(dimension):
-        current_board[i] = "-"
-    game_mines = []
-    while len(game_mines) < game_mines_num:
-        row = rn.randint(0,dimension-1)
-        col = rn.randint(0,dimension-1)
-        if (row,col) not in game_mines:
-            board[row][col]="m"
-            game_mines.append((row,col))
-    sq_size = width // dimension
-
-    return board,current_board,game_mines, sq_size,dimension,mines_count,game_mines_num
 
 def load_images():
     nums = ["0","1","2","3","4","5","6","7","8"]
