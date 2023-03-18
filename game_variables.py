7bb649b7a419358b63ef82116c629ae5c69fe971
Kvothek88 <115589647+Kvothek88@users.noreply.github.com>
2023-03-18 16:41:04 +0200

    Update game_variables.py

--- game_variables.py
+++ game_variables.py
@@ -9,45 +9,47 @@ frame_width = 10
 space = 30
 
 game_type=0
-dimension=9
 difficulty=0
-
-if game_type==0:
-    dimension = 9
-elif game_type==1:
-    dimension=16
-elif game_type==2:
-    dimension = 30
-
 mines_list = [[12,17,30],
             [35,48,88],
             [103,159,266]]
 
-game_mines_num = mines_list[game_type][difficulty]
-mines_count = game_mines_num
-board = np.empty((dimension,dimension),dtype='object')
-current_board = np.empty((dimension,dimension),dtype='object')
-for i in range(dimension):
-    board[i] = "-"
-for i in range(dimension):
-    current_board[i] = "-"
-
-game_mines = []
-
-while len(game_mines) < game_mines_num:
-    row = rn.randint(0,dimension-1)
-    col = rn.randint(0,dimension-1)
-    if (row,col) not in game_mines:
-        board[row][col]="m"
-        game_mines.append((row,col))
-
-sq_size = width // dimension
 fps = 15
 gameover = False
 win=False
 play=False
 options=False
 mainmenu=True
-empty_squares=(dimension**2)-game_mines_num
-opened_squares=[]
 images = {}
+opened_squares=[]
+
+def reset_game(difficulty,game_type,mines_list):
+    if game_type==0:
+        dimension = 9
+    elif game_type==1:
+        dimension=16
+    elif game_type==2:
+        dimension = 30
+    game_mines_num = mines_list[game_type][difficulty]
+    mines_count = game_mines_num
+    board = np.empty((dimension,dimension),dtype='object')
+    current_board = np.empty((dimension,dimension),dtype='object')
+    for i in range(dimension):
+        board[i] = "-"
+    for i in range(dimension):
+        current_board[i] = "-"
+    game_mines = []
+    while len(game_mines) < game_mines_num:
+        row = rn.randint(0,dimension-1)
+        col = rn.randint(0,dimension-1)
+        if (row,col) not in game_mines:
+            board[row][col]="m"
+            game_mines.append((row,col))
+    sq_size = width // dimension
+
+    return board,current_board,game_mines, sq_size,dimension,mines_count,game_mines_num
+
+
+board,current_board,game_mines,sq_size,dimension,mines_count,game_mines_num = reset_game(difficulty,game_type,mines_list)
+
+empty_squares = (dimension**2)-game_mines_num
